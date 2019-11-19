e2f26d045a03dc3e6664b81117b5a3acd47d0032
Sune L. Sørensen <sonnysune@msn.com>
2019-11-19 11:31:47 +0100

    Added more service clients

--- src/system_manager/src/system_manager.cpp
+++ src/system_manager/src/system_manager.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include "ros/ros.h"
 
+//Services:
+#include "robot_control/goto_config.h"
+
+//Topics:
+
+
 using namespace std;
 
-int currentOrderContents[3]; //b, r, y
+#define BLUE_BRICKS   0
+#define RED_BRICKS    1
+#define YELLOW_BRICKS 2
+
+#define BRICK_NOMATCH 0
+#define BRICK_MATCH   1
+
+//Order info:
+int currentOrderContents[3]; //blue, red, yellow
 bool boxContainsOrder[4] = {false, false, false, false};
+int currentOrderId;
+int currentOrderTicket;
+
+
 int currentBox;
 string currentBoxString;
 bool running = true;
@@ -14,6 +32,20 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "system_manager");
     ros::NodeHandle n;
 
+    //Setup service clients:
+    ros::ServiceClient robotClient = n.serviceClient<robot_control::goto_config>("/go_to_config");
+    robot_control::goto_config robCmd;
+
+    //ros::ServiceClient mesGetClient = n.serviceClient<mes_ordering::GetOrder_srv>("/MES_GetOrder");
+    //mes_ordering::GetOrder_srv mesCmd;
+
+    //ros::ServiceClient mesDelClient = n.serviceClient<mes_ordering::DeleteOrder_srv>("/MES_DeleteOrder");
+
+    ros::ServiceClient visClient = n.serviceClient<vision::check_brick>("/check_brick");
+    vision::check_brick visCmd;
+
+
+    //Start packing:
     ROS_INFO("System manager started!");
     ros::Duration(1).sleep();
 
@@ -27,14 +59,20 @@ int main(int argc, char** argv)
         currentOrderContents[1] = 2;
         currentOrderContents[2] = 3;
 
+        //Find the next available box:
         currentBox = 0;
-        while (boxContainsOrder[currentBox])
+        while (boxContainsOrder[currentBox] && currentBox < 4)
             currentBox++;
 
-        if(currentBox < 3)
+        if(currentBox == 3) //Start packing in the last box
         {
-            ROS_ERROR("All boxes are filled with orders!");
-            return -1;
+            ROS_INFO("Packing in last box, calling MiR");
+            //Call MiR
+        }
+        else if(currentBox > 3)
+        {
+            ROS_INFO("Waiting for MiR...");
+            //Wait for MiR
         }
 
         switch (currentBox)
@@ -53,27 +91,93 @@ int main(int argc, char** argv)
             break;
         
         default:
-            ROS_ERROR("Invalid box was selected. Program error.");
+            ROS_ERROR("Invalid box was selected. Program error, please debug.");
             return -1;
-            break;
         }
 
         ROS_INFO_STREAM("Packing in box " << currentBoxString);
 
-        while(currentOrderContents[0] > 0) //Pack all the blue bricks..
+        while(currentOrderContents[BLUE_BRICKS] > 0) //Pack all the blue bricks
         {
-            //Ask robot to go to preSmall, then graspSmall, then grasp
+            //Ask robot to go to preSmall, then graspSmall (auto-grasps)
+
+            //Ask vision system to validate brick:
+            visCmd.request.color = "blue";
+
+            if(!visClient.call(visCmd))
+            {
+                ROS_ERROR("Vision service call failed!");
+                return -1;
+            }
+
+            //Ask robot to go to preSmall
+
+            if(visCmd.response.result == BRICK_MATCH)
+            {
+                //Ask robot to go to currentBoxString
+                //Ask vision system to validate brick:
+                if(!visClient.call(visCmd))
+                {
+                    ROS_ERROR("Vision service call failed!");
+                    return -1;
+                }
+
+                //Ask robot to open gripper
+
+                if(visCmd.response.result == BRICK_MATCH)
+                    currentOrderContents[BLUE_BRICKS]--; //One less brick to pack :)
+                else
+                    ROS_ERROR("No brick to drop!"); 
+            }
+            else
+            {
+                //Ask robot to go to aboveDiscard
+                //Ask robot to open gripper
+            }
+        }
+
+        while(currentOrderContents[RED_BRICKS] > 0) //Pack all the red bricks
+        {
+            //Ask robot to go to preMedium, then graspMedium, then grasp
 
             //Ask vision system to validate brick
 
+            //Ask robot to go to preMedium
+
             if(true /*brick is validated*/)
             {
-                //Ask robot to go to preSmall, then currentBoxString
+                //Ask robot to go to currentBoxString
                 //Ask vision system to validate brick
                 //Ask robot to open gripper
 
                 if(true /*brick is validated*/)
-                    currentOrderContents[0]--; //One less brick to pack :)
+                    currentOrderContents[RED_BRICKS]--; //One less brick to pack :)
+                else
+                    ROS_ERROR("No brick to drop!"); 
+            }
+            else
+            {
+                //Ask robot to go to aboveDiscard
+                //Ask robot to open gripper
+            }
+        }
+
+        while(currentOrderContents[YELLOW_BRICKS] > 0) //Pack all the yellow bricks
+        {
+            //Ask robot to go to preLarge, then graspLarge, then grasp
+
+            //Ask vision system to validate brick
+
+            //Ask robot to go to preLarge
+
+            if(true /*brick is validated*/)
+            {
+                //Ask robot to go to currentBoxString
+                //Ask vision system to validate brick
+                //Ask robot to open gripper
+
+                if(true /*brick is validated*/)
+                    currentOrderContents[YELLOW_BRICKS]--; //One less brick to pack :)
                 else
                     ROS_ERROR("No brick to drop!"); 
             }
@@ -84,6 +188,11 @@ int main(int argc, char** argv)
             }
             
         }
+
+        //Now the order should be packed, notify MES-node:
+        //Service-call to MES-node
+
+        //Call MiR
             
     }
 
