6fa5a4054cbd0331155c3b8aa52fbe4c2261c78e
ibsgaard <ibsgaardfik@gmail.com>
2019-11-19 13:49:10 +0100

    mir_api node

---
+++ src/mir_api/scripts/mir_api_service.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+from mir_api.srv import mir_api_action, mir_api_actionResponse
+import requests
+import time
+import rospy
+
+
+language = {'Accept-Language': "en_US"}
+
+
+class RestMiR():
+    def __init__(self):
+        self.authorization = {
+            'Authorization': "Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=="}
+        self.HOST = 'http://10.10.19.40/api/v2.0.0/'
+    #We need to put the name of our mission
+    def get_mission(self, mission_name="MISSION_NAME"):
+        response = requests.get(self.HOST + 'missions', headers=self.authorization)
+        mission = ""
+        if response.status_code != 200:
+            print(response.status_code)
+        for counter in response.json():
+            if counter["name"] == mission_name:
+                mission = counter
+                print(mission)
+        return mission
+
+    def add_mission_to_queue(self, mission):
+        r = requests.get(self.HOST + 'mission_queue', headers=self.authorization)
+        data = {"mission_id": str(mission["guid"])}
+        for i in[-1,-2,-3,-4]:
+            #print(data['mission_id'])
+            _id = requests.get(self.HOST + 'mission_queue/'+str(r.json()[i]['id']), headers=self.authorization)
+            #print(_id.json()['mission_id'])
+            if data['mission_id'] == _id.json()['mission_id']:
+                if r.json()[i]['state'] == 'Pending' or  r.json()[i]['state'] == 'Executing':
+                    print("ERROR" + " Already Pending or Executing")
+                    return 0
+        response = requests.post(self.HOST + "mission_queue", json=data, headers=self.authorization)
+        if response.status_code != 201:
+            print("ERROR" + str(response.status_code))
+            return False
+        return True
+
+    #In the mission we will have to set coils (plc registers) in order to get information if robot has docked and etc.
+    def read_register(self, register_id):
+        response = requests.get(self.HOST + 'registers/' + str(register_id), headers=self.authorization)
+        register_value = 0
+        if response.status_code != 200:
+            print(response.status_code)
+            print(response.text)
+
+        if (response.json()['id'] == register_id):
+            register_value = response.json()['value']
+        return register_value
+
+    #As above, we can set the register when loading on the robot is ready to move to main table
+    def write_register(self, register_id, value):
+        data = {"value": value}
+        response = requests.put(self.HOST + 'registers/' + str(register_id), json = data, headers=self.authorization)
+
+        if response.status_code != 200:
+            print(response.status_code)
+        return 0
+
+
+
+robot = RestMiR()
+
+def handle_GotoGr8(req):
+    if req.action == 1:
+        guid = robot.get_mission("GoToGr8")
+        if robot.add_mission_to_queue(guid):
+            return mir_api_actionResponse(1)
+    elif req.action == 2:
+        if robot.read_register(8) == 2:
+            return mir_api_actionResponse(2)
+    elif req.action == 3:
+        robot.write_register(8, 1)  # MIR can go
+        return mir_api_actionResponse(3)
+
+    return mir_api_actionResponse(0)
+
+def mir_api_server():
+    rospy.init_node('mir_api_server')
+    s = rospy.Service('mir_api', mir_api_action, handle_GotoGr8)
+    print "Ready The MiR"
+    rospy.spin()
+
+if __name__ == "__main__":
+    mir_api_server()
+
+'''
+robot = RestMiR()
+guid = robot.get_mission("GoToGr5")
+print(robot.add_mission_to_queue(guid))
+while robot.read_register(5) != 2: # wait for MIR to arrive
+    robot.read_register(5)
+# add function to put boxes on MIR & flag to make sure we are done with packing
+print("robot arrived")
+time.sleep(2)
+robot.write_register(5, 1)  # MIR can go
+robot.write_register(1,1)
+print("bye MIR")
+'''
